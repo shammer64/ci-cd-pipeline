735c940b75a00342dcf1e0ecb4fd1fb88fa743fc
Scott Hammer <scott.a.hammer@accenture.com>
2019-03-13 16:02:12 -0500

    Attempt to fix Heroku deploy-release issue

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -4,3 +4,4 @@ heroku plugins:install heroku-container-registry
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:login
 heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
